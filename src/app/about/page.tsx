999082b32daa3d64057ea057f886ff54d22ac743
Jhin3283 <hj3283@gmail.com>
2024-01-03 10:15:12 +0900

    Header,About 추가

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function About() {
+  return (
+    <main>
+      <h1>About Us</h1>
+    </main>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="image not found" />
+      <h1>Welcome NextJS</h1>
+    </>
+  );
+}
